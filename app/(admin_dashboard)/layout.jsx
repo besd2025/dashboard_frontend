e02faf3c8e5757723be901d7a72d1df43a54f266
seseelvis <elvisirdknd16@gmail.com>
2025-06-20 15:52:08 +0200

    new push from Elvis

--- app/(admin_dashboard)/layout.jsx
+++ app/(admin_dashboard)/layout.jsx
@@ -3,8 +3,8 @@ import { ThemeProvider } from "../ui/context/ThemeContext";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
-export default async function AdminLayout({ children }) {
-  const token = await cookies().get("accessToken")?.value;
+export default function AdminLayout({ children }) {
+  const token = cookies().get("accessToken")?.value;
   if (!token) {
     redirect("/"); // redirige vers la page d'accueil si non connecté
   }
