7a22eaee0c0c1a59bef076b2d40c9d71ef47f423
seseelvis <elvisirdknd16@gmail.com>
2025-06-20 16:12:40 +0200

    new push from Elvis

--- app/(admin_dashboard)/layout.jsx
+++ app/(admin_dashboard)/layout.jsx
@@ -4,11 +4,11 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function AdminLayout({ children }) {
-  const cookieStore = await cookies(); // ✅ obligatoire depuis 14.2
-  const token = cookieStore.get("accessToken")?.value;
-  if (!token) {
-    redirect("/"); // redirige vers la page d'accueil si non connecté
-  }
+  // const cookieStore = await cookies();
+  // const token = cookieStore.get("accessToken")?.value;
+  // if (!token) {
+  //   redirect("/"); // redirige vers la page d'accueil si non connecté
+  // }
 
   return (
     <ThemeProvider>
