c31d53ca29eb8e10762c3c908c92af67183190f4
besd2025 <besd.promotion1@gmail.com>
2025-09-05 09:10:43 +0200

    9/5

--- app/ui/ui_elements/tables/dashboard/hangars/details/hangar-pending-cultivators.jsx
+++ app/ui/ui_elements/tables/dashboard/hangars/details/hangar-pending-cultivators.jsx
@@ -226,7 +226,7 @@ function HangarPendingCultivators() {
     <div className="overflow-hidden rounded-2xl border border-gray-200 bg-white px-5 pt-5 dark:border-gray-800 dark:bg-white/[0.03]  sm:px-6 sm:pt-6 ">
       <div className="flex items-center justify-between w-full gap-2 px-3 py-3 border-b  border-gray-200 dark:border-gray-800 sm:gap-4  lg:border-b-0 lg:px-0 lg:py-4">
         <h3 className="text-lg font-semibold text-gray-800 dark:text-white/90">
-          Cultivateurs payés
+          Cultivateurs en attente
         </h3>
         {/* search */}
         <div className="hidden lg:block">
