1b200d7b359d4cfca19c7e9b3f1ee60bbfef4104
besd2025 <besd.promotion1@gmail.com>
2025-11-25 12:03:23 +0200

    11/25

--- app/ui/dashboard/hangars/details/cards_overview.jsx
+++ app/ui/dashboard/hangars/details/cards_overview.jsx
@@ -77,7 +77,7 @@ export default function CardsOverview({ hangar_id }) {
   return (
     <div className="grid grid-cols-1 gap-4 lg:grid-cols-16 md:gap-4">
       {/* <!-- Metric Item Start quantity collected --> */}
-      <div className="rounded-2xl col-span-4 border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
+      <div className="rounded-2xl col-span-2 border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
         <div className="flex items-end justify-between mb-2">
           <div>
             <span className="text-sm text-gray-500 dark:text-gray-400">
@@ -246,10 +246,283 @@ export default function CardsOverview({ hangar_id }) {
       </div>
 
       {/* <!-- Metric Item End --> */}
+      {/* <!-- Metric Item Start --> */}
+      <div className="rounded-2xl col-span-6 flex flex-row justify-between border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
+        <div>
+          <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+            {/* <GroupIcon className="text-gray-800 size-6 dark:text-white/90" /> */}
 
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 text-gray-800  dark:text-white/90"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H6.911a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661Z"
+              />
+            </svg>
+          </div>
+          <div className="flex items-end justify-between mt-2">
+            <div>
+              <span className="text-sm text-gray-500 dark:text-gray-400">
+                Qté Restante
+              </span>
+
+              <h4 className="ml-3  font-semibold text-gray-800 text-lg dark:text-white/90">
+                {ventes.total_blanc + ventes?.total_jaune >= 1000 ? (
+                  <>
+                    {(
+                      (ventes.total_blanc + ventes?.total_jaune) /
+                      1000
+                    ).toLocaleString("fr-FR", {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}{" "}
+                    <span className="text-sm">T</span>
+                  </>
+                ) : (
+                  <>
+                    {(ventes.total_blanc + ventes?.total_jaune)?.toLocaleString(
+                      "fr-FR"
+                    ) || 0}{" "}
+                    <span className="text-sm">Kg</span>
+                  </>
+                )}
+              </h4>
+            </div>
+            {/* <Badge color="success">
+            <ArrowUpIcon />
+            2.0%
+          </Badge> */}
+          </div>
+        </div>
+        <div className="w-px bg-gray-200 h-full dark:bg-gray-800"></div>
+
+        <div className="">
+          <div className="flex items-end justify-between   rounded-2xl">
+            <div>
+              <div className="flex flex-row items-center gap-x-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-8"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Maïs Blanc
+                </span>
+              </div>
+
+              <h4 className=" font-semibold text-gray-800 text-xl dark:text-white/90">
+                {ventes?.total_blanc >= 1000 ? (
+                  <>
+                    {(ventes?.total_blanc / 1000).toLocaleString("fr-FR", {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}{" "}
+                    <span className="text-sm">T</span>
+                  </>
+                ) : (
+                  <>
+                    {ventes?.total_blanc?.toLocaleString("fr-FR") || 0}{" "}
+                    <span className="text-sm">Kg</span>
+                  </>
+                )}
+              </h4>
+            </div>
+          </div>
+          <div className="flex items-end justify-between mt-2 rounded-2xl">
+            <div>
+              <div className="flex flex-row items-center gap-x-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-8 text-yellow-500 "
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Maïs Jaune
+                </span>
+              </div>
+
+              <h4 className=" font-semibold text-yellow-600 text-xl dark:text-white/90">
+                {ventes?.total_jaune >= 1000 ? (
+                  <>
+                    {(ventes?.total_jaune / 1000)?.toLocaleString("fr-FR", {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}{" "}
+                    <span className="text-sm">T</span>
+                  </>
+                ) : (
+                  <>
+                    {ventes?.total_jaune?.toLocaleString("fr-FR") || 0}{" "}
+                    <span className="text-sm">Kg</span>
+                  </>
+                )}
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <!-- Metric Item End --> */}
+      {/* <!-- Metric Item Start --> */}
+      <div className="rounded-2xl col-span-6 flex flex-row justify-between border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
+        <div>
+          <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+            {/* <GroupIcon className="text-gray-800 size-6 dark:text-white/90" /> */}
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 text-gray-800  dark:text-white/90"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m6.75 12-3-3m0 0-3 3m3-3v6m-1.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+              />
+            </svg>
+          </div>
+          <div className="flex items-end justify-between mt-2">
+            <div>
+              <span className="text-sm text-gray-500 dark:text-gray-400">
+                Qté Transferer
+              </span>
+
+              <h4 className="ml-3  font-semibold text-gray-800 text-lg dark:text-white/90">
+                {ventes.total_blanc + ventes?.total_jaune >= 1000 ? (
+                  <>
+                    {(
+                      (ventes.total_blanc + ventes?.total_jaune) /
+                      1000
+                    ).toLocaleString("fr-FR", {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}{" "}
+                    <span className="text-sm">T</span>
+                  </>
+                ) : (
+                  <>
+                    {(ventes.total_blanc + ventes?.total_jaune)?.toLocaleString(
+                      "fr-FR"
+                    ) || 0}{" "}
+                    <span className="text-sm">Kg</span>
+                  </>
+                )}
+              </h4>
+            </div>
+            {/* <Badge color="success">
+            <ArrowUpIcon />
+            2.0%
+          </Badge> */}
+          </div>
+        </div>
+        <div className="w-px bg-gray-200 h-full dark:bg-gray-800"></div>
+
+        <div className="">
+          <div className="flex items-end justify-between   rounded-2xl">
+            <div>
+              <div className="flex flex-row items-center gap-x-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-8"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Maïs Blanc
+                </span>
+              </div>
+
+              <h4 className=" font-semibold text-gray-800 text-xl dark:text-white/90">
+                {ventes?.total_blanc >= 1000 ? (
+                  <>
+                    {(ventes?.total_blanc / 1000).toLocaleString("fr-FR", {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}{" "}
+                    <span className="text-sm">T</span>
+                  </>
+                ) : (
+                  <>
+                    {ventes?.total_blanc?.toLocaleString("fr-FR") || 0}{" "}
+                    <span className="text-sm">Kg</span>
+                  </>
+                )}
+              </h4>
+            </div>
+          </div>
+          <div className="flex items-end justify-between mt-2 rounded-2xl">
+            <div>
+              <div className="flex flex-row items-center gap-x-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="size-8 text-yellow-500 "
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+                <span className="text-sm text-gray-500 dark:text-gray-400">
+                  Maïs Jaune
+                </span>
+              </div>
+
+              <h4 className=" font-semibold text-yellow-600 text-xl dark:text-white/90">
+                {ventes?.total_jaune >= 1000 ? (
+                  <>
+                    {(ventes?.total_jaune / 1000)?.toLocaleString("fr-FR", {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}{" "}
+                    <span className="text-sm">T</span>
+                  </>
+                ) : (
+                  <>
+                    {ventes?.total_jaune?.toLocaleString("fr-FR") || 0}{" "}
+                    <span className="text-sm">Kg</span>
+                  </>
+                )}
+              </h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <!-- Metric Item End --> */}
       {/* <!-- Metric Item Start --> quantity bought */}
 
-      <div className="rounded-2xl col-span-6 flex flex-row justify-between border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
+      <div className="rounded-2xl col-span-5 flex flex-row justify-between border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
         <div>
           <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
             {/* <GroupIcon className="text-gray-800 size-6 dark:text-white/90" /> */}
@@ -385,9 +658,7 @@ export default function CardsOverview({ hangar_id }) {
       </div>
 
       {/* <!-- Metric Item End --> */}
-      {/* <!-- Metric Item Start --> */}
 
-      {/* <!-- Metric Item End --> */}
       {/* <!-- Metric Item Start --> */}
 
       <div className="rounded-2xl col-span-4 border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
@@ -432,7 +703,7 @@ export default function CardsOverview({ hangar_id }) {
       {/* <!-- Metric Item End --> */}
       {/* <!-- Metric Item Start --> */}
 
-      <div className="rounded-2xl col-span-4 border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
+      <div className="rounded-2xl col-span-3 border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
         <div className="flex items-end justify-between mb-2">
           <div>
             <span className="text-sm text-gray-500 dark:text-gray-400">
