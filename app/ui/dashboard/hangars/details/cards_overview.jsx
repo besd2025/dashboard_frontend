f8df58b56a460616d5eef091da66ade0faa49037
besd2025 <besd.promotion1@gmail.com>
2025-11-26 09:56:02 +0200

    11/26

--- app/ui/dashboard/hangars/details/cards_overview.jsx
+++ app/ui/dashboard/hangars/details/cards_overview.jsx
@@ -96,39 +96,6 @@ export default function CardsOverview({ hangar_id }) {
   }, [hangar_id]);
   return (
     <div className="grid grid-cols-1 gap-4 lg:grid-cols-16 md:gap-4">
-      {/* <!-- Metric Item Start quantity collected --> */}
-      <div className="rounded-2xl col-span-2 border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
-        <div className="flex items-end justify-between mb-2">
-          <div>
-            <span className="text-sm text-gray-500 dark:text-gray-400">
-              Cultivateurs
-            </span>
-          </div>
-        </div>
-        <div className="flex">
-          {/* <GroupIcon className="text-gray-800 size-6 dark:text-white/90" /> */}
-          <div className="flex items-center justify-center p-2 bg-gray-100 rounded-xl dark:bg-gray-800">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="size-6 text-gray-600  dark:text-white/70"
-            >
-              <path
-                fillRule="evenodd"
-                d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z"
-                clipRule="evenodd"
-              />
-              <path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" />
-            </svg>
-          </div>
-
-          <h4 className="ml-3 font-bold text-gray-800 text-lg dark:text-white/90">
-            {cultivateurs?.nbr_cultivators || 0}
-          </h4>
-        </div>
-      </div>
-      {/* <!-- Metric Item End --> */}
       {/* <!-- Metric Item Start --> */}
 
       <div className="rounded-2xl col-span-6 flex flex-row justify-between border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
@@ -679,7 +646,7 @@ export default function CardsOverview({ hangar_id }) {
       {/* <!-- Metric Item End --> */}
       {/* <!-- Metric Item Start --> quantity bought */}
 
-      <div className="rounded-2xl col-span-5 flex flex-row justify-between border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
+      <div className="rounded-2xl col-span-6 lg:col-span-5 flex flex-row justify-between border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
         <div>
           <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
             {/* <GroupIcon className="text-gray-800 size-6 dark:text-white/90" /> */}
@@ -818,7 +785,7 @@ export default function CardsOverview({ hangar_id }) {
 
       {/* <!-- Metric Item Start --> */}
 
-      <div className="rounded-2xl col-span-4 border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
+      <div className="rounded-2xl col-span-6 lg:col-span-3 border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
         <div className="flex items-end justify-between mb-2">
           <div>
             <span className="text-sm text-gray-500 dark:text-gray-400">
@@ -860,7 +827,7 @@ export default function CardsOverview({ hangar_id }) {
       {/* <!-- Metric Item End --> */}
       {/* <!-- Metric Item Start --> */}
 
-      <div className="rounded-2xl col-span-3 border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
+      <div className="rounded-2xl col-span-6 lg:col-span-3 border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
         <div className="flex items-end justify-between mb-2">
           <div>
             <span className="text-sm text-gray-500 dark:text-gray-400">
@@ -903,7 +870,7 @@ export default function CardsOverview({ hangar_id }) {
       {/* <!-- Metric Item End --> */}
       {/* <!-- Metric Item Start --> */}
 
-      <div className="rounded-2xl col-span-4 border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
+      <div className="rounded-2xl col-span-6 lg:col-span-3 border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
         <div className="flex items-end justify-between mb-2">
           <div>
             <span className="text-sm text-gray-500 dark:text-gray-400">
@@ -955,6 +922,39 @@ export default function CardsOverview({ hangar_id }) {
         </div>
       </div>
 
+      {/* <!-- Metric Item End --> */}
+      {/* <!-- Metric Item Start quantity collected --> */}
+      <div className="rounded-2xl col-span-6 lg:col-span-2 border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-4">
+        <div className="flex items-end justify-between mb-2">
+          <div>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Cultivateurs
+            </span>
+          </div>
+        </div>
+        <div className="flex">
+          {/* <GroupIcon className="text-gray-800 size-6 dark:text-white/90" /> */}
+          <div className="flex items-center justify-center p-2 bg-gray-100 rounded-xl dark:bg-gray-800">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="size-6 text-gray-600  dark:text-white/70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z"
+                clipRule="evenodd"
+              />
+              <path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" />
+            </svg>
+          </div>
+
+          <h4 className="ml-3 font-bold text-gray-800 text-lg dark:text-white/90">
+            {cultivateurs?.nbr_cultivators || 0}
+          </h4>
+        </div>
+      </div>
       {/* <!-- Metric Item End --> */}
     </div>
   );
