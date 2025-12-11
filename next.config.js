c5295bbf7284f273e4ea5653965b9487fa5f2f28
besd2025 <besd.promotion1@gmail.com>
2025-12-11 15:34:52 +0200

    12/11

--- next.config.js
+++ next.config.js
@@ -1,17 +1,15 @@
 const imageDomain = process.env.NEXT_PUBLIC_IMAGE_DOMAIN;
+
 const nextConfig = {
   images: {
     domains: imageDomain ? [imageDomain] : [],
-
-    // Optionnel : si vous voulez aussi autoriser des patterns spécifiques
-    // remotePatterns: [
-    //   {
-    //     protocol: 'http',
-    //     hostname: '192.168.88.33',
-    //     pathname: '/media/cultivators/photos/**',
-    //   },
-    // ],
   },
+
+  // Force Webpack instead of Turbopack
+  experimental: {
+    turbo: false,
+  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
